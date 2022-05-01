6c6e86dbc896307d8dc453d128fc0fcd0d30669c
firatkill <firatkill@icloud.com>
2022-05-02 01:27:25 +0300

    Done

--- app.js
+++ app.js
@@ -4,24 +4,62 @@ const newcategory = document.querySelector("#category");
 const select = document.querySelector("#category-select");
 const answer = document.querySelector("#answer");
 
-let random = getRandom(`${select.options[select.selectedIndex].textContent}`);
+let livesLeft = 10;
+let random;
 // Event listeners
-document.querySelector("#category-select").onchange = () => {
-  answer.innerHTML = "";
-  
+document.addEventListener("DOMContentLoaded", () => {
+  random = getRandom(`Şehir`);
+  newMaker();
+});
 
-  for (i = 0; i < random.length; i++) {
-    answer.innerHTML += `<span id="${i}">_</span>`;
-  }
-};
+document.querySelector("#category-select").onchange = newMaker;
 
 document.querySelector(".grid-item").onclick = control;
 
 // Functions
 
+function newMaker() {
+  answer.innerHTML = "";
+
+  random = getRandom(`${select.options[select.selectedIndex].textContent}`);
+  console.log(random);
+  for (i = 0; i < random.length; i++) {
+    answer.innerHTML += `<span id="${i}">_</span>`;
+  }
+}
+
+let index;
 function control(e) {
-  console.log(e.target.dataset.key);
-  if()
+  index = random.indexOf(e.target.dataset.key);
+  if (random.indexOf(e.target.dataset.key) > -1) {
+    for (i = 0; i < random.length; i++) {
+      if (random[i] == e.target.dataset.key) {
+        document.getElementById(`${i}`).textContent = random[i];
+        if (answer.textContent == random) {
+          win();
+        }
+      }
+    }
+    console.log(index);
+  } else {
+    livesLeft--;
+    document.querySelector(
+      "#lives"
+    ).textContent = `You have ${livesLeft} lives left. `;
+    if (livesLeft == 0) {
+      lose();
+    }
+  }
+}
+function win() {
+  document.querySelector("#body").innerHTML =
+    "<h1 class='live'>You Saved Him</h1>";
+  document.querySelector("#body").style.backgroundColor = "Yellow";
+}
+function lose() {
+  document.querySelector("#body").innerHTML =
+    "<h1 class='dead'>You Made Him Die</h1>";
+  document.querySelector("#body").style.backgroundColor = "black";
 }
 
 function getFromLocalStorage(key) {
